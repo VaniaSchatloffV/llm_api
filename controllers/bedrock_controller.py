e0cb251d3956045b4fa1e1bf00429a142df51965
José Ignacio Alegria <jose.alegria@sansano.usm.cl>
2024-07-31 17:37:49 -0400

    prompt v2

--- controllers/bedrock_controller.py
+++ controllers/bedrock_controller.py
@@ -7,7 +7,7 @@ from botocore.exceptions import ClientError
 from helpers import conversation_helper
 
 
-def invoke_llm(messages: list, system_prompt: str, temperature=0.1, top_p=0.9):
+def invoke_llm(messages: list, system_prompt: str, temperature=0.1, top_p=0.1):
     """
     Invokes Anthropic Claude 3 Sonnet to run an inference using the input
     provided in the request body.
@@ -20,7 +20,7 @@ def invoke_llm(messages: list, system_prompt: str, temperature=0.1, top_p=0.9):
         service_name="bedrock-runtime", region_name="us-east-1"
     )
     # Invoke Claude 3 with the text prompt
-    model_id = "anthropic.claude-v2:1"
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
     body = json.dumps(
                 {
                     "anthropic_version": "bedrock-2023-05-31",
@@ -58,7 +58,34 @@ def send_prompt(prompt: str, conversation_id: int, user_id: int):
     conversation_helper.insert_message(conversation_id, "user", prompt)
     messages = conversation_helper.get_messages(conversation_id)
     
-    response = invoke_llm(messages=messages, system_prompt="reemplaza todas las vocales de tu respuesta con la letra 'A'")
+    #systemPrompt = """Eres un traductor de lenguaje natural a consultas SQL, no respondas con más que la consulta generada, siempre y cuando te den informacion suficiente para hacer una consulta, caso contrario necesito que digas que 
+    #necesitas más informacion para poder formular una consulta. No puedes generar consultas que alteren la base de datos, solo SELECTS.
+    #Los nombres se guardan en 'nombres', los apellidos en 'sur', el tipo de cancer en 'tipo_cancer', y finalmente clasificacion del cancer en 'categoria_cancer', y la llave primaria es la 'id'. Todo esto 
+    #se encuentra en la tabla que se llama pacientes. Estas son las unicas columnas que existen en las tablas, si te dan informacion que no se te entrega, no la coloques en la consulta."""
+
+
+    #Context
+    #reference text
+    #instrucciones/describir la estructura de la salida
+    
+    systemPrompt = """
+    Las siguientes son descripciones de una tabla y sus campos en una base de datos:
+
+    Una de las tablas a utilizar es pacientes, esta almacena pacientes cuyos nombres se guardan en el campo 'nombre', sus apellidos en 'sur', la zona que afecta el cáncer 
+    en 'tipo_cancer', y la severidad del cancer es un numero del 1 al 5 almacenado en el campo 'categoria_cancer'. Además, la primary key de la tabla es el campo 'id'.
+    Otra tabla a usar es doctores, esta almacena información de doctores de FALP, cuyor nombres se guardan en el campo 'nombre', sus apellidos en 'sur', su especialización 
+    en 'especialización', y el hospital donde trabaja en 'hospital'. Además, la primary key de la tabla es el campo 'id'.
+    Estas dos tablas tienen una tabla intermedia, llamada 'atenciones', esta muestra los pacientes que son tratados por cada doctor con relación varios a varios. Sus campos
+    son 'id_atencion', 'id_doctor', tomada desde la tabla 'doctores', y 'id_paciente', tomada desde la tabla 'pacientes'.
+
+    Con esta información, necesito que traduzcas consultas en lenguaje natural a consultas SQL.
+    No respondas con nada más que el SQL generado, un ejemplo de esto es: "SELECT * FROM pacientes;", fijate como NO hay '\n' en la respuesta. Tampoco agregues cordialidades o explicaciones, responde solo con SQL.
+    Si se te pide informacion que no esta en la tabla no la agregues a la consulta, responde lo que puedas, pero no des explicaciones, responde solo con SQL.
+    Si se te pide modificar la base de datos, indica que no lo tienes permitido, este es el unico caso donde puedes no usar SQL.
+
+    """
+
+    response = invoke_llm(messages=messages, system_prompt=systemPrompt)
     # invoca llm
     conversation_helper.insert_message(conversation_id, "assistant", response)
     # retorna estructura para leer desde backend-frontend
