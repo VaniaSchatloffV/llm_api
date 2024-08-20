29ce8be7d4156350fbba1d84d33eb652b44bb429
Vania Schatloff <v.schatloff@gmail.com>
2024-08-19 21:42:52 -0400

    agregando iteración en descarga de archivos

--- api/files.py
+++ api/files.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse, StreamingResponse
 from pydantic import BaseModel
 from helpers import file_helper
+import os
+from io import BytesIO
 
 router = APIRouter()
 
@@ -10,9 +12,15 @@ class File(BaseModel):
 
 
 @router.get("/download/{file_id}/csv")
-def download_csv(file_id):
-    return FileResponse(file_helper.get_file_csv_name(file_id), media_type="csv")
+def download_csv(file_id: str):
+    return StreamingResponse(file_helper.download_csv_file(file_id),
+                             media_type="csv",
+                             headers={"Content-Disposition": f"attachment; filename={os.path.basename(file_helper.get_file_csv_name(file_id))}"})
 
 @router.get("/download/{file_id}/xlsx")
-def download_csv(file_id):
-    return FileResponse(file_helper.get_file_xlsx_name(file_id), media_type="xlsx")
+def download_xlsx(file_id: int):
+    return StreamingResponse(
+        file_helper.download_xlsx_file(file_id),
+        media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        headers={"Content-Disposition": f"attachment; filename={os.path.basename(file_helper.get_file_xlsx_name(file_id))}"}
+    )
