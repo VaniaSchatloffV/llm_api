0d939fc890e53528f4e16ea7e59b59fa301b31fc
José Ignacio Alegria <jose.alegria@sansano.usm.cl>
2024-08-23 18:03:37 -0400

    Test Integracion RAG

--- controllers/bedrock_controller.py
+++ controllers/bedrock_controller.py
@@ -3,11 +3,104 @@ import json
 import numpy as np
 import pandas as pd
 
+#RAG
+from langchain_community.llms import Bedrock
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import FAISS
+
 from botocore.exceptions import ClientError
 from helpers import conversation_helper
 
+#RAG
+my_data = [
+    """
+    En la tabla de nombre: "pacientes"
+    se almacena información referente a personas que padecen diferentes tipos de cáncer atendidas por la Fundación oncológica Arturo López Pérez.
+    La información almacenada comprende las columnas:
+    'id' de tipo int: esta es la clave primaria de la tabla.
+    'nombre' de tipo varchar: este campo corresponde al nombre del paciente. Algunos ejemplos de entradas son: 'Juan', 'Ana', 'Pablo', 'Laura', todos parten con mayuscula.
+    'sur' de tipo varchar: este campo  corresponde al apellido del paciente. Algunos ejemplos de entradas son: 'Perez', 'Gomez', 'Martinez', todos parten con mayuscula.
+    'tipo_cancer' de tipo varchar: corresponde a la zona afectada del paciente. Algunos ejemplos de entradas son: 'Cáncer de Mama', 'Cáncer de Pulmón', 'Cáncer de Colon', 'Cáncer de Prostata', todos parten con mayuscula y llevan tilde.
+    'categoria_cancer' de tipo int: da un numero del 1 al 5 respecto a la severidad.
+    """,
 
-def invoke_llm(messages: list, system_prompt: str, temperature=0.1, top_p=0.1):
+    """
+    En la tabla de nombre: "doctores"
+    se almacena información referente a doctores que trabajan en la Fundación oncológica Arturo López Pérez.
+    La información almacenada comprende las columnas:
+    'id' de tipo int: esta es la clave primaria de la tabla.
+    'nombre' de tipo varchar: este campo corresponde al nombre del doctor. Algunos ejemplos de entradas son: 'Juan', 'Ana', 'Pablo', 'Laura', todos parten con mayuscula.
+    'sur' de tipo varchar: este campo  corresponde al apellido del doctor. Algunos ejemplos de entradas son: 'Perez', 'Gomez', 'Martinez', todos parten con mayuscula.
+    'especialización' de tipo varchar: este campo contiene la especialidad del doctor. Ejemplos incluyen: 'Oncología', 'Pediatría', 'Ginecología', todos parten con mayúscula y llevan tildes.
+    'hospital' de tipo varthcar: este campo contiene donde practica el doctor. Ejemplos de entradas incluyen: 'Hospital Central', 'Hospital del Norte' y 'Hospital del Sur', todos parten con mayuscula.
+    """,
+
+    """
+    En la tabla de nombre "atenciones"
+    es una tabla intermedia para las tablas 'pacientes' y 'doctores' donde se almacena información referente a qué doctor ha atendido a qué paciente.
+    Las entradas siempre son uno a uno, vale decir en cada entrada un paciente es atendido por un doctor
+    La información almacenada comprende las columnas:
+    'id' de tipo int: esta es la clave primaria de la tabla.
+    'id_doctor' de tipo int: es clave foránea de la tabla "doctores".
+    'id_paciente' de tipo int: es clave foránea de la tabla "pacientes".
+
+    """]
+
+
+def invoke_llm(messages: list, temperature=0, top_p=0.1):
+    question = "¿Cuál es el tipo de cáncer más común entre los pacientes atendidos en el hospital del Norte??"
+
+    bedrock = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
+
+    model = Bedrock(model_id="anthropic.claude-v2:1", client=bedrock) # Revisar modelos
+
+    bedrock_embeddings = BedrockEmbeddings(model_id="amazon.titan-embed-text-v1", client=bedrock) # Revisar modelos
+
+    # create vector store
+    vector_store = FAISS.from_texts(my_data, bedrock_embeddings)
+
+    # create retriever
+    retriever = vector_store.as_retriever(
+        search_kwargs={"k": 3}  # maybe we can add a score threshold here?
+    )
+
+    results = retriever.get_relevant_documents(question)
+
+    results_string = []
+    for result in results:
+        results_string.append(result.page_content)
+    
+    # build template:
+    template = ChatPromptTemplate.from_messages(
+        [
+            (
+
+                "system",
+                """Las siguientes son descripciones de una tabla y sus campos en una base de datos:
+            
+                {context}.
+                
+                Con esta información, necesito que traduzcas consultas en lenguaje natural a consultas SQL.
+                No respondas con nada más que el SQL generado, un ejemplo de SQL es: "SELECT * FROM pacientes;", fijate como NO hay '\n' en la respuesta. Tampoco agregues cordialidades o explicaciones, responde solo con SQL.
+                Si se te pide informacion que no esta en la tabla no la agregues a la consulta, responde lo que puedas, pero no des explicaciones, responde solo con SQL.
+                Si se te pide modificar la base de datos, indica que no lo tienes permitido, este es el único caso donde puedes no usar SQL.
+                """
+            ),
+            (
+                "user",
+                "{input}"
+            ),
+        ]
+    )
+
+    chain = template.pipe(model)
+
+    response = chain.invoke({"input": question, "context": results_string})
+    return response
+
+
+def invoke_llm_viejo(messages: list, system_prompt: str, temperature=0.1, top_p=0.1):
     """
     Invokes Anthropic Claude 3 Sonnet to run an inference using the input
     provided in the request body.
@@ -58,35 +151,9 @@ def send_prompt(prompt: str, conversation_id: int, user_id: int):
     conversation_helper.insert_message(conversation_id, "user", prompt)
     messages = conversation_helper.get_messages(conversation_id)
     
-    #systemPrompt = """Eres un traductor de lenguaje natural a consultas SQL, no respondas con más que la consulta generada, siempre y cuando te den informacion suficiente para hacer una consulta, caso contrario necesito que digas que 
-    #necesitas más informacion para poder formular una consulta. No puedes generar consultas que alteren la base de datos, solo SELECTS.
-    #Los nombres se guardan en 'nombres', los apellidos en 'sur', el tipo de cancer en 'tipo_cancer', y finalmente clasificacion del cancer en 'categoria_cancer', y la llave primaria es la 'id'. Todo esto 
-    #se encuentra en la tabla que se llama pacientes. Estas son las unicas columnas que existen en las tablas, si te dan informacion que no se te entrega, no la coloques en la consulta."""
 
-
-    #Context
-    #reference text
-    #instrucciones/describir la estructura de la salida
-    
-    systemPrompt = """
-    Las siguientes son descripciones de una tabla y sus campos en una base de datos:
-
-    Una de las tablas a utilizar es pacientes, esta almacena pacientes cuyos nombres se guardan en el campo 'nombre', sus apellidos en 'sur', la zona que afecta el cáncer 
-    en 'tipo_cancer', y la severidad del cancer es un numero del 1 al 5 almacenado en el campo 'categoria_cancer'. Además, la primary key de la tabla es el campo 'id'.
-    Otra tabla a usar es doctores, esta almacena información de doctores de FALP, cuyor nombres se guardan en el campo 'nombre', sus apellidos en 'sur', su especialización 
-    en 'especialización', y el hospital donde trabaja en 'hospital'. Además, la primary key de la tabla es el campo 'id'.
-    Estas dos tablas tienen una tabla intermedia, llamada 'atenciones', esta muestra los pacientes que son tratados por cada doctor con relación varios a varios. Sus campos
-    son 'id_atencion', 'id_doctor', tomada desde la tabla 'doctores', y 'id_paciente', tomada desde la tabla 'pacientes'.
-
-    Con esta información, necesito que traduzcas consultas en lenguaje natural a consultas SQL.
-    No respondas con nada más que el SQL generado, un ejemplo de esto es: "SELECT * FROM pacientes;", fijate como NO hay '\n' en la respuesta. Tampoco agregues cordialidades o explicaciones, responde solo con SQL.
-    Si se te pide informacion que no esta en la tabla no la agregues a la consulta, responde lo que puedas, pero no des explicaciones, responde solo con SQL.
-    Si se te pide modificar la base de datos, indica que no lo tienes permitido, este es el unico caso donde puedes no usar SQL.
-
-    """
-
-    response = invoke_llm(messages=messages, system_prompt=systemPrompt)
+    response = invoke_llm(messages=messages)
     # invoca llm
     conversation_helper.insert_message(conversation_id, "assistant", response)
     # retorna estructura para leer desde backend-frontend
-    return {"response": response[0]["text"], "conversation_id": conversation_id}
+    return {"response": response, "conversation_id": conversation_id}
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker build -t api_image .
 docker rm api
-docker run -p 8007:8007 -v $(pwd):/app --env-file .env --name api --network mynetwork api_image
+docker run -p 8007:8007 -v C:/Users/Cototo/Desktop/u/llm_api:/app --env-file .env --name api --network mynetwork api_image
