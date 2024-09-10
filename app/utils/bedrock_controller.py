f4af1c701fedfa612d4f40bbf03e919ea84d7ef5
Vania Schatloff <v.schatloff@gmail.com>
2024-09-09 21:19:13 -0300

    cambiando nombre variable

--- app/utils/bedrock_controller.py
+++ app/utils/bedrock_controller.py
@@ -23,7 +23,7 @@ def execute_query(query):
     except Exception as e:
         return {"data": None, "error": str(e)}
 
-def send_prompt_and_process(prompt: str, conversation_id: int, user_id: int):
+def send_prompt_and_process(user_message: str, conversation_id: int, user_id: int):
     # si no existe la conversación, se crea y retorna nuevo id
 
     if conversation_id == 0:
@@ -35,24 +35,24 @@ def send_prompt_and_process(prompt: str, conversation_id: int, user_id: int):
     }
 
     last_option_message = conversation_helper.get_option_messages(conversation_id)
-    if prompt in file_helper.OPTIONS and last_option_message and last_option_message.get("role") == "assistant":
-        conversation_helper.insert_message(conversation_id, "user", prompt, "option")
+    if user_message in file_helper.OPTIONS and last_option_message and last_option_message.get("role") == "assistant":
+        conversation_helper.insert_message(conversation_id, "user", user_message, "option")
         query = conversation_helper.get_last_query(conversation_id)
         with DB_ORM_Handler() as db:
             data = db.query(query, return_data=True)
-        file_id = file_helper.to_file(prompt, data)
-        resp = {"text": "El archivo ya está listo", "file_id": file_id, "file_type": prompt}
+        file_id = file_helper.to_file(user_message, data)
+        resp = {"text": "El archivo ya está listo", "file_id": file_id, "file_type": user_message}
         conversation_helper.insert_message(conversation_id, "assistant", resp, "file")
         return {"response": resp, "conversation_id": conversation_id}
     else:
-        conversation_helper.insert_message(conversation_id, "user", prompt)
+        conversation_helper.insert_message(conversation_id, "user", user_message)
     
     # Se obtienen mensajes anteriores para la llm
     messages = conversation_helper.get_messages_for_llm(conversation_id)
     messages_for_llm = llm_helper.format_llm_memory(messages)
 
 
-    classifier = llm_helper.LLM_Identify_NL(prompt, messages_for_llm)
+    classifier = llm_helper.LLM_Identify_NL(user_message, messages_for_llm)
 
     if classifier != "SQL":
         conversation_helper.insert_message(conversation_id, "assistant", classifier)
@@ -60,7 +60,7 @@ def send_prompt_and_process(prompt: str, conversation_id: int, user_id: int):
         return response_format
     
 
-    resp = llm_helper.invoke_llm(question=prompt, messages=messages)
+    resp = llm_helper.invoke_llm(question=user_message, messages=messages)
     # Verificacion del mensaje
     verification = llm_helper.LLM_recognize_SQL(resp.get("answer"))
     if verification == "NL":
@@ -76,7 +76,7 @@ def send_prompt_and_process(prompt: str, conversation_id: int, user_id: int):
             success = False
             for i in range(retry):
                 error = db_response.get("error")
-                query = llm_helper.LLM_Fix_SQL(prompt, query, error)
+                query = llm_helper.LLM_Fix_SQL(user_message, query, error)
                 conversation_helper.insert_message(conversation_id, "assistant", query, "query_review")
                 db_response = execute_query(query)
                 if db_response.get("error") is None:
@@ -95,7 +95,7 @@ def send_prompt_and_process(prompt: str, conversation_id: int, user_id: int):
         option_msg = "¿En qué formato desea recibir la información?"
 
         if len(tokens_used) < 500: #REVISAR cantidad Y VER SI LO LIMPIAMOS
-            nl_complete_response = llm_helper.LLM_Translate_Data_to_NL(data, prompt, query) + " " + option_msg 
+            nl_complete_response = llm_helper.LLM_Translate_Data_to_NL(data, user_message, query) + " " + option_msg
             response_format["response"] = {"text": nl_complete_response, "options": file_helper.OPTIONS}
             conversation_helper.insert_message(conversation_id, "assistant", response_format.get("response"), "option")
             
