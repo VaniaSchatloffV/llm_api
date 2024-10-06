4434e91af5811b1840977d16e279ece421b89219
José Ignacio Alegria <jose.alegria@sansano.usm.cl>
2024-10-05 23:12:10 -0300

    se cambia logica para que las descargas sean independientes de la extensión

--- app/utils/helpers/file_helper.py
+++ app/utils/helpers/file_helper.py
@@ -47,76 +47,53 @@ def to_excel(data: list):
     df.to_excel(file_path)
     return file_name
 
-def get_file_csv_name(file_id: int):
+def get_file_path(file_id: int):
     """
-    Retorna string con la ruta de archivo csv
+    Retorna string con la ruta de archivo
     """
-    with DB_ORM_Handler as db:
-        query = "SELECT name FROM archivos WHERE id = " + file_id
-        file_name = db.query(query, return_data=True)
-
-    file_path = settings.temp_files + "/" +  str(file_name) + ".csv"
-    return file_path
-
-def get_file_xlsx_name(file_id: int):
-    """
-    Retorna string con la ruta de archivo xlsx
-    """
-    with DB_ORM_Handler as db:
-        query = "SELECT name FROM archivos WHERE id = " + file_id
-        file_name = db.query(query, return_data=True)
-
-    file_path = settings.temp_files + "/" +  str(file_name) + ".xlsx"
-    return file_path
+    with DB_ORM_Handler() as db:
+        query = "SELECT name, extension FROM archivos WHERE id = " + str(file_id) #revisar si agregar esto a env
+        res = db.query(query, return_data=True)
+        if res:
+            file_name = res[0].get("name") #tomar primer elemento de una lista con forma [{'name': 'str', 'extension': 'str'}], debe haber una mejor manera
+            file_ext = res[0].get("extension")
+            file_path = settings.temp_files + "/" +  str(file_name) + "." + str(file_ext)
+            return file_path
+        else:
+            raise HTTPException(status_code=404, detail="File not found")
 
 def file_iterator(file_path: str):
     """
     Itera sobre archivo
     """
-    with open(file_path, mode="rb") as file:
-        yield from file
+    if "csv" in file_path: #revisar como cambiar esto para que sea más modular y menos duro
+        with open(file_path, mode="rb") as file:
+            yield from file
+    elif "xlsx" in file_path:
+        with open(file_path, "rb") as excel_file:
+            while chunk := excel_file.read(8192):  # 8 KB
+                yield chunk
     os.remove(file_path)
+    
 
-def excel_iterator(file_path: str) -> Iterator[bytes]:
+def download_file(file_id: int) -> BytesIO:
     """
-    Itera sobre archivo excel
+    Descarga archivo por chunks
     """
-    with open(file_path, "rb") as excel_file:
-        while chunk := excel_file.read(8192):  # 8 KB
-            yield chunk
-    os.remove(file_path)
-
-
-def download_csv_file(file_id: int):
-    """
-    Descarga archivo csv por chunks
-    """
-    file_path = get_file_csv_name(file_id)
+    file_path = get_file_path(file_id)
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return file_iterator(file_path)
 
-def download_xlsx_file(file_id: int) -> BytesIO:
-    """
-    Descarga archivo xlsx por chunks
-    """
-    file_path = get_file_xlsx_name(file_id)
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
-    return excel_iterator(file_path)
-
 def csv_to_excel(file_id: int):
-    file_path = get_file_csv_name(file_id)
+    file_path = get_file_path(file_id)
     read_file_product = pd.read_csv (file_path)
     read_file_product.to_excel (settings.temp_files + "/" + str(file_id) + ".xlsx", index = None, header=True)
     os.remove(file_path)
     return file_id
 
-def file_exists(file_id: str, file_type: str):
-    if file_type == "csv":
-        file_path = get_file_csv_name(file_id)
-    elif file_type == "xlsx":
-        file_path = get_file_xlsx_name(file_id)
+def file_exists(file_id: str):
+    file_path = get_file_path(file_id)
     return os.path.isfile(file_path)
 
 def new_file(user_id: int, conversation_id: int, name: str, extension: str):
