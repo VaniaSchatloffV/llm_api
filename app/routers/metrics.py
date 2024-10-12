b335a4475b27207323fa3a32b9784a90259a368c
Vania Schatloff <v.schatloff@gmail.com>
2024-10-12 16:37:42 -0300

    agregando métricas, fix files, fix mensajes

--- app/routers/metrics.py
+++ app/routers/metrics.py
@@ -4,7 +4,7 @@ from app.utils.helpers import metrics_helper
 from app.utils.auth import TokenData, verify_token
 import os
 
-from app.schemas.metrics import Metric
+from app.schemas.metrics import Metric, MetricTable
 
 router = APIRouter()
 
@@ -16,5 +16,5 @@ def download(metric: Metric, token_data: TokenData = Depends(verify_token)):
     metrics_helper.upload_metric(conversation_id, questions, calification)
 
 @router.get("/getTable/")
-def get_metric_table():
-    return metrics_helper.get_metric_table()
+def get_metric_table(metricTable: MetricTable):
+    return metrics_helper.get_metric_table(limit=metricTable.limit, offset=metricTable.offset, order_by=metricTable.order_by, order_way=metricTable.order_way)
--- app/utils/helpers/metrics_helper.py
+++ app/utils/helpers/metrics_helper.py
@@ -47,30 +47,36 @@ def get_table(offset: Optional[int] = None, limit: Optional[int] = None, order_b
             order = [ConversationObject.id.asc()]
         else:
             order = [ConversationObject.id.desc()]
+    if order_by == "metric_id":
+        if order_way == "asc":
+            order = [MetricObject.id.asc()]
+        else:
+            order = [MetricObject.id.desc()]
     with DB_ORM_Handler() as db:
         results = db.getObjects(
             MetricObject,
-            columns = [MetricObject.id,ConversationObject.id,ConversationObject.user_id,MetricObject.calification,MetricObject.data],
+            columns = [
+                MetricObject.id.label("Id métrica"),
+                ConversationObject.user_id.label("Id usuario"),
+                ConversationObject.id.label("Id conversación"),
+                MetricObject.calification.label("Calificación (valor máximo 5)"),
+                MetricObject.data.label("Preguntas")
+                ],
             join_conditions=[(ConversationObject, MetricObject.conversation_id == ConversationObject.id)],
             order_by=order,
-            limit=limit
+            limit=limit,
+            offset=offset
         )
             
     return results
 
 
-def get_metric_table(limit: Optional[int] = None, offset: Optional[int] = None, order_by: Optional[str] = None, order_way: Optional[str] = None):
-    # total = count_metrics()
-    # if offset >= total:
-    #     next_offset = None
-    # else:
-    #     next_offset = offset + limit
-    #     if next_offset >= total:
-    #         next_offset = None
-    offset = None
-    limit = None
-    order_by = None
-    order_way = None
-    total = None
-    next_offset = None
+def get_metric_table(limit: Optional[int] = None, offset: Optional[int] = 0, order_by: Optional[str] = None, order_way: Optional[str] = None):
+    total = count_metrics()
+    if offset >= total:
+        next_offset = None
+    else:
+        next_offset = offset + limit
+        if next_offset >= total:
+            next_offset = None
     return {"data" : get_table(offset, limit, order_by, order_way), "total": total, "next_offset": next_offset}
