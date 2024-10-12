3ce237b9b560a0b242f80eb1bd1b51d854202965
Vania Schatloff <v.schatloff@gmail.com>
2024-10-12 14:08:56 -0300

    agregando condicion para tener logs debug

--- app/logger.py
+++ app/logger.py
@@ -1,7 +1,11 @@
+from app.dependencies import get_settings
 import logging, sys
+settings = get_settings()
+env = settings.environment
 
 LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
+if env == "dev":
+    LOGGER.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler(sys.stdout)
 log_formatter = logging.Formatter("%(asctime)s  [%(levelname)s]: %(message)s")
 stream_handler.setFormatter(log_formatter)
