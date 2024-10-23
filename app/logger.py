fe4c8a98ac5ccd993da27b65c566876d9a13d4f8
José Ignacio Alegria <jose.alegria@sansano.usm.cl>
2024-10-23 13:42:49 -0300

    logging a archivo

--- app/logger.py
+++ app/logger.py
@@ -3,6 +3,12 @@ import logging, sys
 settings = get_settings()
 env = settings.environment
 
+logging.basicConfig(
+    filename="C:\\Users\\Cototo\\Desktop\\llm_api\\logging_output\\logging.log", #revisar nombre y ubicacion lol
+    filemode="a",
+    format="%(asctime)s  [%(levelname)s]: %(message)s"
+)
+
 LOGGER = logging.getLogger(__name__)
 if env == "dev":
     LOGGER.setLevel(logging.DEBUG)
